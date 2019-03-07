8b1b5ceaca69a2c28a97fcbf03bfb4f5f4ada1ae
LotusMel <46916651+LotusMel@users.noreply.github.com>
2019-03-07 03:17:38 -0600

    got method to return string

--- Sherlock/Program.cs
+++ Sherlock/Program.cs
@@ -6,32 +6,41 @@ namespace Sherlock
     {
         public static void Main()
         {
-            string press = { "a reporter from the Times", "a reporter from the Observer" };
-            string family = {"Mycroft Holmes", "Mrs. Hudson" };
-            string enemies = {"Professor Moriarty", "Charles "Augustus" Milverton", "John Woodley" };
-            string loveInterests = {"Irene Adler" };
-            string friends = {"Inspector Lestrade", "Dr. Watson" };
+            string[] press = { "a reporter from the Times", "a reporter from the Observer" };
+            string[] family = {"Mycroft Holmes", "Mrs. Hudson" };
+            string[] enemies = {"Professor Moriarty", "Charles", "Augustus", "Milverton", "John Woodley" };
+            string[] loveInterests = {"Irene Adler" };
+            string[] friends = {"Inspector Lestrade", "Dr. Watson" };
 
             Console.WriteLine(KnockKnock(press));
             Console.WriteLine(KnockKnock(family));
             Console.WriteLine(KnockKnock(enemies));
             Console.WriteLine(KnockKnock(loveInterests));
             Console.WriteLine(KnockKnock(friends));
+            Console.ReadLine();
+
+            
+
+
         }
 
-        private static string KnockKnock(string[] guests)
+        private static string KnockKnock(string [] guests)
         {
-            foreach (string guest in guests)
+            string [] messages = { "Enter", "Go Away! (sound of violin music...)" };
+
+            foreach (string guest in messages)
             {
-                if (guest == "Dr Watson" || guest == "Inspector Lestrade")
+                if (guest == "Dr. Watson" || guest == "Mrs. Hudson")
                 {
-                    return "Enter";
+                    return messages[0];
                 }
-                else
+                else 
+               
                 {
-                    return "Go Away! (sound of violin music...)";
+                    return messages[1];
                 }
             }
+            return "";
         }
     }
 }
